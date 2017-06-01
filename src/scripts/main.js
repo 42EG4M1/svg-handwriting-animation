d75aaf8f5cdcce7ab9feee8542bd45cc981e8ebe
tatsuya azegami <theorthodoxworks@gmail.com>
2017-06-02 02:22:05 +0900

    Modify css修正 + wait画面追加

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@
 import PlayHandwriting from './modules/PlayHandwriting';
 import replay from './modules/replay';
 
+const wait = document.getElementById('js-wait');
 const path1 = Array.from(document.querySelectorAll('#js-mask path'));
 const path2 = Array.from(document.querySelectorAll('#js-clip path'));
 const btn1 = document.getElementById('btn1');
@@ -19,6 +20,7 @@ const ph2 = new PlayHandwriting(path2, 15, 0);
 
 function resetAll() {
   return new Promise((resolve, reject) => {
+    wait.classList.add('fadeout');
     PlayHandwriting.reset(path1);
     PlayHandwriting.reset(path2);
   });
@@ -26,10 +28,10 @@ function resetAll() {
 
 const init = () => {
   resetAll()
-  .then(ph1.playAnimation())
-  .then(setTimeout(() => btn1.classList.toggle('hidden'), 2600))
-  .then(setTimeout(() => ph2.playAnimation(), 2600))
-  .then(setTimeout(() => btn2.classList.toggle('hidden'), 5200));
+  .then(setTimeout(() => ph1.playAnimation(), 1000))
+  .then(setTimeout(() => btn1.classList.toggle('hidden'), 3600))
+  .then(setTimeout(() => ph2.playAnimation(), 3600))
+  .then(setTimeout(() => btn2.classList.toggle('hidden'), 6200));
 };
 
 window.onload = () => init();
